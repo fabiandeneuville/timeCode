54304c0c5cbc5cd96b275c824d94a012439fbdca
Deneuville Fabian <deneuville.fabian@live.fr>
2022-07-09 19:57:20 +0200

    Restarting from scratch

--- public/app.js
+++ public/app.js
@@ -8,8 +8,6 @@ const rate = document.querySelector('#rate');
 const calculateBtn = document.querySelector('.calculate-btn');
 const items = document.querySelectorAll('.time-code-item');
 const resultDisplay = document.querySelector('.result');
-let visible;
-visible = true;
 let rateValue;
 rateValue = 24;
 inputs.forEach((input) => {
@@ -38,89 +36,3 @@ function changeMax(input) {
     input.setAttribute("max", `${rateValue}`);
 }
 // CALCULATE FUNCTION
-function calculate(items) {
-    let tempResultH, tempResultM, tempResultS, tempResultI;
-    tempResultH = 0;
-    tempResultM = 0;
-    tempResultS = 0;
-    tempResultI = 0;
-    let resultH, resultM, resultS, resultI;
-    items.forEach((item) => {
-        let errorMsg = item.querySelector('.error-msg');
-        let hoursInput = item.querySelector('.time-code-h-input');
-        let minutesInput = item.querySelector('.time-code-m-input');
-        let secondsInput = item.querySelector('.time-code-s-input');
-        let imagesInput = item.querySelector('.time-code-i-input');
-        let tempH, tempM, tempS, tempI;
-        if (parseInt(hoursInput.value) < 0 || parseInt(hoursInput.value) > 24 || parseInt(minutesInput.value) < 0 || parseInt(minutesInput.value) > 60 || parseInt(secondsInput.value) < 0 || parseInt(secondsInput.value) > 60 || parseInt(imagesInput.value) < 0 || parseInt(imagesInput.value) > rateValue) {
-            errorMsg.textContent = "Please set a valid value !";
-        }
-        else {
-            errorMsg.textContent = "";
-            tempH = parseInt(hoursInput.value) | 0;
-            tempM = parseInt(minutesInput.value) | 0;
-            tempS = parseInt(secondsInput.value) | 0;
-            tempI = parseInt(imagesInput.value) | 0;
-            tempResultH += tempH;
-            tempResultM += tempM;
-            tempResultS += tempS;
-            tempResultI += tempI;
-        }
-    });
-    if (tempResultI < 10) {
-        resultI = "0" + tempResultI;
-    }
-    else if (tempResultI >= rateValue) {
-        tempResultS += 1;
-        tempResultI = tempResultI - rateValue;
-        resultI = "0" + tempResultI.toString();
-    }
-    else {
-        resultI = tempResultI.toString();
-    }
-    if (tempResultS < 10) {
-        resultS = "0" + tempResultS;
-    }
-    else if (tempResultS >= 60) {
-        tempResultM += 1;
-        tempResultS = tempResultS - 60;
-        resultS = "0" + tempResultS.toString();
-    }
-    else {
-        resultS = tempResultS.toString();
-    }
-    if (tempResultM < 10) {
-        resultM = "0" + tempResultM;
-    }
-    else if (tempResultM >= 60) {
-        tempResultH += 1;
-        tempResultM = tempResultM - 60;
-        resultM = "0" + tempResultM.toString();
-    }
-    else {
-        resultM = tempResultM.toString();
-    }
-    if (tempResultH < 10) {
-        resultH = "0" + tempResultH;
-    }
-    else {
-        resultH = tempResultH.toString();
-    }
-    if (Number(resultH) > 24) {
-        resultDisplay.textContent = 'Limit reached for hours value !';
-    }
-    else {
-        resultDisplay.textContent = `${resultH}:${resultM}:${resultS}:${resultI}`;
-    }
-}
-calculateBtn.addEventListener('click', () => {
-    calculate(items);
-});
-function displayBtn() {
-    if (visible) {
-        calculateBtn.style.display = "block";
-    }
-    else {
-        calculateBtn.style.display = "none";
-    }
-}
--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,6 @@ const calculateBtn = document.querySelector('.calculate-btn') as HTMLButtonEleme
 const items = document.querySelectorAll('.time-code-item') as NodeListOf<HTMLDivElement>
 const resultDisplay = document.querySelector('.result') as HTMLParagraphElement;
 
-let visible: boolean;
-visible = true;
-
 let rateValue : number;
 rateValue = 24;
 
@@ -48,93 +45,3 @@ function changeMax(input: HTMLInputElement){
 }
 
 // CALCULATE FUNCTION
-
-function calculate(items: NodeListOf<HTMLDivElement>){
-
-    let tempResultH : number, tempResultM : number, tempResultS : number, tempResultI : number;
-    tempResultH = 0
-    tempResultM = 0;
-    tempResultS = 0;
-    tempResultI = 0;
-    let resultH: String, resultM: String, resultS: String, resultI: String;
-
-    items.forEach((item: HTMLDivElement) => {
-        let errorMsg = item.querySelector('.error-msg') as HTMLSpanElement;
-        let hoursInput = item.querySelector('.time-code-h-input') as HTMLInputElement;
-        let minutesInput = item.querySelector('.time-code-m-input') as HTMLInputElement;
-        let secondsInput = item.querySelector('.time-code-s-input') as HTMLInputElement;
-        let imagesInput = item.querySelector('.time-code-i-input') as HTMLInputElement;
-
-        let tempH: number, tempM: number, tempS: number, tempI: number;
-
-        if(parseInt(hoursInput.value) < 0 || parseInt(hoursInput.value) > 24 || parseInt(minutesInput.value) < 0 || parseInt(minutesInput.value) > 60 || parseInt(secondsInput.value) < 0 || parseInt(secondsInput.value) > 60 || parseInt(imagesInput.value) < 0 || parseInt(imagesInput.value) > rateValue) {
-            errorMsg.textContent = "Please set a valid value !"
-        } else {
-            errorMsg.textContent = "";
-            tempH = parseInt(hoursInput.value) | 0;
-            tempM = parseInt(minutesInput.value) | 0;
-            tempS = parseInt(secondsInput.value) | 0;
-            tempI = parseInt(imagesInput.value) | 0;
-    
-            tempResultH += tempH;
-            tempResultM += tempM;
-            tempResultS += tempS;
-            tempResultI += tempI;
-        }
-    })
-
-    if(tempResultI < 10){
-        resultI = "0"+tempResultI;
-    } else if(tempResultI >= rateValue){    
-        tempResultS += 1;
-        tempResultI = tempResultI - rateValue;
-        resultI = "0"+tempResultI.toString();
-    } else {
-        resultI = tempResultI.toString();
-    }
-
-    if(tempResultS < 10){
-        resultS = "0"+tempResultS;
-    } else if(tempResultS >= 60){    
-        tempResultM += 1;
-        tempResultS = tempResultS - 60;
-        resultS = "0"+tempResultS.toString();
-    } else {
-        resultS = tempResultS.toString();
-    }
-
-    if(tempResultM < 10){
-        resultM = "0"+tempResultM;
-    } else if(tempResultM >= 60){    
-        tempResultH += 1;
-        tempResultM = tempResultM - 60;
-        resultM = "0"+tempResultM.toString();
-    } else {
-        resultM = tempResultM.toString();
-    }
-
-    if(tempResultH < 10){
-        resultH = "0"+tempResultH;
-    } else {
-        resultH = tempResultH.toString();
-    }
-
-    if(Number(resultH) > 24){
-        resultDisplay.textContent = 'Limit reached for hours value !'
-    } else {
-        resultDisplay.textContent = `${resultH}:${resultM}:${resultS}:${resultI}`;
-
-    }
-}
-
-calculateBtn.addEventListener('click', () => {
-    calculate(items);
-})
-
-function displayBtn(){
-    if(visible){
-        calculateBtn.style.display = "block"
-    } else {
-        calculateBtn.style.display = "none"
-    }
-}
