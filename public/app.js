a85f925a7028ef6ab4ab57d208b90339fdea2a21
Deneuville Fabian <deneuville.fabian@live.fr>
2022-07-09 17:58:39 +0200

    Adding a function to set placeholder, min and max attributes

--- public/app.js
+++ public/app.js
@@ -1,32 +1,38 @@
 "use strict";
 const inputs = document.querySelectorAll('input');
-const imgRateInputs = document.querySelectorAll('.time-code-i-input');
+const hInputs = document.querySelectorAll('.time-code-h-input');
+const mInputs = document.querySelectorAll('.time-code-m-input');
+const sInputs = document.querySelectorAll('.time-code-s-input');
+const imgInputs = document.querySelectorAll('.time-code-i-input');
 const rate = document.querySelector('#rate');
 const calculateBtn = document.querySelector('.calculate-btn');
 const items = document.querySelectorAll('.time-code-item');
 const resultDisplay = document.querySelector('.result');
+let visible;
+visible = true;
 let rateValue;
 rateValue = 24;
 inputs.forEach((input) => {
     setPlaceHolder(input);
 });
-imgRateInputs.forEach((input) => {
-    setMinMax(input);
-});
+hInputs.forEach((input) => setMinMax(input, 0, 24));
+mInputs.forEach((input) => setMinMax(input, 0, 60));
+sInputs.forEach((input) => setMinMax(input, 0, 60));
+imgInputs.forEach((input) => setMinMax(input, 0, rateValue));
 rate.addEventListener("change", () => {
     rateValue = parseInt(rate.value);
-    imgRateInputs.forEach((input) => {
+    imgInputs.forEach((input) => {
         changeMax(input);
     });
 });
-// ATTRIBUTES SETING FUNCTIONS
+// ATTRIBUTES (placeholder, min, max) SETTING FUNCTIONS
 function setPlaceHolder(input) {
     input.setAttribute("placeholder", "00");
 }
 ;
-function setMinMax(input) {
-    input.setAttribute("min", "0");
-    input.setAttribute("max", `${rateValue}`);
+function setMinMax(input, min, max) {
+    input.setAttribute("min", `${min}`);
+    input.setAttribute("max", `${max}`);
 }
 function changeMax(input) {
     input.setAttribute("max", `${rateValue}`);
@@ -110,3 +116,11 @@ function calculate(items) {
 calculateBtn.addEventListener('click', () => {
     calculate(items);
 });
+function displayBtn() {
+    if (visible) {
+        calculateBtn.style.display = "block";
+    }
+    else {
+        calculateBtn.style.display = "none";
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 const inputs = document.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
-const imgRateInputs = document.querySelectorAll('.time-code-i-input') as NodeListOf<HTMLInputElement>;
+
+const hInputs = document.querySelectorAll('.time-code-h-input') as NodeListOf<HTMLInputElement>
+const mInputs = document.querySelectorAll('.time-code-m-input') as NodeListOf<HTMLInputElement>
+const sInputs = document.querySelectorAll('.time-code-s-input') as NodeListOf<HTMLInputElement>
+const imgInputs = document.querySelectorAll('.time-code-i-input') as NodeListOf<HTMLInputElement>;
+
 const rate = document.querySelector('#rate') as HTMLInputElement;
 const calculateBtn = document.querySelector('.calculate-btn') as HTMLButtonElement;
 const items = document.querySelectorAll('.time-code-item') as NodeListOf<HTMLDivElement>
 const resultDisplay = document.querySelector('.result') as HTMLParagraphElement;
+
+let visible: boolean;
+visible = true;
 
 let rateValue : number;
 rateValue = 24;
@@ -12,26 +20,27 @@ inputs.forEach((input: HTMLInputElement) => {
     setPlaceHolder(input);
 })
 
-imgRateInputs.forEach((input: HTMLInputElement) => {
-    setMinMax(input);
-})
+hInputs.forEach((input: HTMLInputElement) => setMinMax(input, 0, 24));
+mInputs.forEach((input: HTMLInputElement) => setMinMax(input, 0, 60));
+sInputs.forEach((input: HTMLInputElement) => setMinMax(input, 0, 60));
+imgInputs.forEach((input: HTMLInputElement) => setMinMax(input, 0, rateValue));
 
 rate.addEventListener("change", () => {
     rateValue = parseInt(rate.value);
-    imgRateInputs.forEach((input: HTMLInputElement) => {
+    imgInputs.forEach((input: HTMLInputElement) => {
         changeMax(input)
     })
 })
 
-// ATTRIBUTES SETING FUNCTIONS
+// ATTRIBUTES (placeholder, min, max) SETTING FUNCTIONS
 
 function setPlaceHolder(input : HTMLInputElement){
     input.setAttribute("placeholder", "00")
 };
 
-function setMinMax(input: HTMLInputElement){
-    input.setAttribute("min", "0");
-    input.setAttribute("max", `${rateValue}`)
+function setMinMax(input: HTMLInputElement, min : number, max: number){
+    input.setAttribute("min", `${min}`);
+    input.setAttribute("max", `${max}`)
 }
 
 function changeMax(input: HTMLInputElement){
@@ -120,4 +129,12 @@ function calculate(items: NodeListOf<HTMLDivElement>){
 
 calculateBtn.addEventListener('click', () => {
     calculate(items);
-}) 
+})
+
+function displayBtn(){
+    if(visible){
+        calculateBtn.style.display = "block"
+    } else {
+        calculateBtn.style.display = "none"
+    }
+}
